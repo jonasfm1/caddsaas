54a3448cc5265f707f4b641244a9902fd5156939
jonas_ferutcci <jonas.ferutcci@gmail.com>
2024-07-06 03:22:39 -0400

    removing redundancy

--- back-cadd/login/index.php
+++ back-cadd/login/index.php
@@ -5,18 +5,20 @@
 
     $user_name = mysqli_real_escape_string($conn,$_POST['user-system']);
     $user_password = mysqli_real_escape_string($conn,$_POST['user-password']);
+    $user_company = mysqli_real_escape_string($conn,$_POST['user-company']);
 
     $query = "SELECT name, company, email FROM users WHERE name='$user_name' AND password='$user_password'";
-
     $result = mysqli_query($conn, $query);
     $row_founded = mysqli_num_rows($result);
-    $count = mysqli_num_rows($result);
 
-    if($count == 1){
+    if($row_founded > 0){
         $_SESSION['user_system'] = $user_name;
-        echo var_dump($_SESSION['user_system']);
-    }
+        $_SESSION['user_company'] = $user_company;
 
-    echo "Connected Successfully <br>";
+        echo var_dump($_SESSION['user_system']);
+        echo var_dump($_SESSION['user_company']);
+        
+        // header("location:welcome.php");
+    }
 
     CloseCon($conn);
